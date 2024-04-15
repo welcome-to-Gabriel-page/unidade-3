a3519fb4161a967eba2e3ed8369ba7c060a524af
Gabriel Paiva <136768448+welcome-to-Gabriel-page@users.noreply.github.com>
2024-04-15 12:36:13 -0300

    att.lg

--- src/components/Button.jsx
+++
@@ -1,7 +0,0 @@
-export function Button() {
-    return(
-        <div>
-            <button className="btn">Calcular</button>
-        </div>
-    );
-}
--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -1,24 +1,64 @@
 import '../styles/style-module.css';
-import { Button } from './Button.jsx';
+import { useState } from 'react';
 
 export function Text() {
+        const [nome, setNome] = useState(' ');
+        const [nota1, setNota1] = useState(0);
+        const [nota2, setNota2] = useState(0);
+        const [media, setMedia] = useState(null);
+      
+        const calcularMedia = () => {
+
+          const num1 = parseFloat(nota1);
+          const num2 = parseFloat(nota2);
+
+
+          if (!isNaN(num1) && !isNaN(num2)) {
+            const resultadoMedia = (num1 + num2) / 2;
+            setMedia(resultadoMedia);
+          } else {
+            setMedia(null);
+          }
+        };
+
     return(
         <div>
             <h2 className='title'>Sistema de Notas</h2>
             <div className="container-text">
                 <label htmlFor="name">Nome: </label>
-                <input type="text" id="name" className='entrada'/>
+                <input type="text"
+                value={nome}
+                onChange={(event) => setNome(event.target.value)}
+                id="name"
+                className='entrada'/>
             </div>
             <div className="container-text">
                 <label htmlFor="notaum">Nota 1: </label>
-                <input type="text" id="notaum" className='entrada'/>
+                <input type="text"
+                value={nota1}
+                onChange={(event) => setNota1(event.target.value)}
+                id="notaum"
+                className='entrada'/>
             </div>
             <div className="container-text">
                 <label htmlFor="notadois">Nota 2: </label>
-                <input type="text" id="notadois" className='entrada'/>
+                <input type="text"
+                value={nota2}
+                onChange={(event) => setNota2(event.target.value)}
+                id="notadois" 
+                className='entrada'/>
             </div>
 
-            <Button/>
+            <button className="btn" onClick={calcularMedia}>Calcular</button>
+
+            <div className="container-text">
+                <label>Média Final: </label>
+                <input type="text"
+                value={media}
+                onChange={(event) => setMedia(event.target.value)}
+                id="notadois" 
+                className='entrada'/>
+            </div>
         </div> 
     );
 }
